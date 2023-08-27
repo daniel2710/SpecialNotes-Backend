2974b74b5d252f4d3ac9a216d516b7e571849bd0
daniel2710 <daniel2710es@gmail.com>
2023-08-27 13:40:03 -0500

    edit colors background notes

--- src/schemas/settingsNotes.ts
+++ src/schemas/settingsNotes.ts
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 export const settingsNotesSchema = new mongoose.Schema({
     archived: { type: Boolean, required: false, default: false },
     pinned: { type: Boolean, required: false, default: false },
-    backgroundColor: { type: String, default: 'default', enum: ["default", "black", "white", "red"] },
+    backgroundColor: { type: String, default: 'rich-black', enum: ["rich-black", "cherry-red", "cliff-blue", "qahvei-brown"] },
 })
 
 export const settingsNotesModel = mongoose.model("settingsNotes", settingsNotesSchema)
 
+ 
